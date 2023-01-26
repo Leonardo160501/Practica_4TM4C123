860cf16c6699e8dbe0e4d1fbc70f6f6783012a1c
Leonardo160501 <leonardoyo007@outlook.com>
2023-01-25 18:35:37 -0600

    Deberia jalar

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-    uint16_t Result[2];
+    uint16_t Result[6];
     char ADC[4];
     Configurar_PLL(_50MHZ);  //Confiuracion de velocidad de reloj
     Configura_Reg_ADC0();
@@ -10,20 +10,19 @@ int main(void)
     while(1)
     {
         ADC0_InSeq2(Result);
-        utoa(Result[0],ADC,10); 
+        utoa(Result[0],ADC,10);
         printString(ADC);
-        ADC[0] = '0';
-        ADC[1] = '0';
-        ADC[2] = '0';
-        ADC[3] = '0';   
-        SysTick_espera_1ms(100);
+        utoa(Result[1],ADC,10);
+        printString(ADC);
+        utoa(Result[2],ADC,10);
+        printString(ADC);
+        utoa(Result[3],ADC,10);
+        printString(ADC); 
         ADC0_InSeq1(Result);
-        utoa(Result[1],ADC,10); 
-        printString(ADC);   
-        ADC[0] = '0';
-        ADC[1] = '0';
-        ADC[2] = '0';
-        ADC[3] = '0';         
+        utoa(Result[4],ADC,10); 
+        printString(ADC);
+        utoa(Result[5],ADC,10);
+        printString(ADC);      
 
     }
     
